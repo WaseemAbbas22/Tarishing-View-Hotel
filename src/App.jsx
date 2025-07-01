f370d21dd181e0fc802111aef896206c3f68eb60
Waseem Abbas <Waseemabbasce22@gmail.com>
2025-07-01 15:19:24 +0500

    Updated package-lock.json, App.jsx, index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 
 // PAGES
 import Home from "./pages/Home";
@@ -12,8 +13,15 @@ import Dining from "./pages/Dining";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+//ANIMATION
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 function App() {
+  useEffect(() => {
+  AOS.init({ duration: 1000 });
+}, []);
   return (
     <>
      
