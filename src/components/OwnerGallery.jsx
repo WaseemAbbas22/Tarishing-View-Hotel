35a2b368a9fa4053eb04106d9932ddedc0aada81
Waseem Abbas <Waseemabbasce22@gmail.com>
2025-07-01 15:53:46 +0500

    Updated index.html title and modified OwnerGallery.jsx

--- src/components/OwnerGallery.jsx
+++ src/components/OwnerGallery.jsx
@@ -19,7 +19,7 @@ const OwnerGallery = () => {
   return (
     <section className="px-6 md:px-20 py-12 bg-white">
       <h2 className="text-3xl font-bold text-center mb-8 text-gray-800">
-        Moments with Our Owner
+        Moments Become Memories
       </h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
